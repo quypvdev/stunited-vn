fd1baad481dffc9baa344ab58022bcf4d0607dc0
Nguyễn Thành Trung <116870926+trungiu@users.noreply.github.com>
2022-12-19 19:20:05 +0700

    feat: #stdigital (#2)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 //Layouts
 import Home from '../pages/Home/index';
-const publicRoutes = [{ path: '/', component: Home }];
+import StDigitalPage from '../pages/STDigital/stDigital';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/st-digital', component: StDigitalPage },
+];
 
 const privateRoutes = [];
 
