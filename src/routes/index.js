20a3b8d265632f917bb157b71c586ff64228e317
Nguyễn Thành Trung <116870926+trungiu@users.noreply.github.com>
2022-12-20 09:54:22 +0700

    feat: #portforlio (#7)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 //Layouts
 import Home from '../pages/Home/index';
 import StDigitalPage from '../pages/STDigital/stDigital';
+import Portfolio from '../pages/Portfolio/portforlio';
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/st-digital', component: StDigitalPage },
+    { path: '/portfolio', component: Portfolio },
 ];
 
 const privateRoutes = [];
